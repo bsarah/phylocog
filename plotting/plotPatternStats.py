cfc9e50e949faf07930021b2144d4e8a39a568cf
Sarah <berkemer@lix.polytechnique.fr>
2024-09-19 09:57:03 +0200

    plotting xgroup pie charts for patterns

---
+++ plotting/plotPatternStats.py
@@ -0,0 +1,150 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import matplotlib.dates as mdates
+import matplotlib.colors as mcolors
+import argparse
+import seaborn as sns
+
+#input: input = patternstats file
+#output: output folder for pie charts for single COGs
+#and barplot file to be plotted at the end showing number of COGs per Xgroup
+
+#format: xgroup\tcog
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--infile")#
+parser.add_argument("-c", "--cog", help="cog ID")
+parser.add_argument("-o", "--outfolder") #
+parser.add_argument("-b", "--barplot") #name for barplot text file to be plotted at the end
+args = parser.parse_args()
+
+
+inputfile = ""
+if args.infile:
+    inputfile = args.infile
+else:
+    print("no inputfile given!\n")
+    exit
+
+incog = ""
+if args.cog:
+    incog = args.cog
+else:
+    print("no inputcog given!\n")
+    exit
+
+outputfolder = ""
+if args.outfolder:
+    outputfolder = args.outfolder
+else:
+    print("no outputfolder given!\n")
+    exit
+
+
+
+outputbarplot = ""
+if args.barplot:
+    outputbarplot = args.barplot
+else:
+    print("no output barplot given!\n")
+    exit
+
+
+
+#plan:
+#- keep patternstats file per cog and write another script analysing all the files
+#- create pie charts with percentages of xgroups per COG
+#- and barplot with xaxis=xgroups and yaxis = counts of COGs, which xgroups occurs in how many cogs
+
+
+#read patternstats file
+pats = pd.read_csv(inputfile,sep='\t')
+
+cog2xlist = dict()
+x2coglist = dict()
+for (index,row) in pats.iterrows():
+    cogid = row[1]
+    ps = row[0].split('_')
+    for p in ps:
+        pps = p.split('.')
+        curxgroup = pps[0]
+        if curxgroup in x2coglist:
+            x2coglist[curxgroup].append(cogid)
+        else:
+            x2coglist[curxgroup] = [cogid]
+        if cogid in cog2xlist:
+            cog2xlist[cogid].append(curxgroup)
+        else:
+            cog2xlist[cogid] = [curxgroup]
+
+
+#open barplottextfile appending text
+#transform x2coglist into lines
+#xgroup\tcogid
+outf = open(outputbarplot,"a")
+#prepare data for pie chart
+#print(x2coglist)
+#plot barplot with bar height=number of same cogid
+#problem: no insight into type of structural domain, e.g. L, NC etc
+xgs = []
+numcogs = []
+for (k,v) in x2coglist.items():
+    if(k=="ENDEND"):
+        continue
+    xgs.append(str(k))
+    numcogs.append(len(v))
+    outf.write(f'{k}\t{inputcog}\n')
+    
+    
+df = pd.DataFrame()
+df['xgroup'] = xgs
+df['counts'] = numcogs
+#dataset = pd.DataFrame({'xgroup': xgs, 'numcogs': numcogs}, columns=['xgroup', 'numcogs'])
+#print(df)
+#df[:5]
+
+#calculate percentages for xgroups
+sumcogs = sum(numcogs)
+percogs = []
+
+xgs_filtered = []
+percogs_filtered = []
+for n in range(len(numcogs)):
+    perc = round(numcogs[n]/sumcogs,2)
+    percogs.append(perc)
+    if(perc > 0):
+        xgs_filtered.append(xgs[n])
+        percogs_filtered.append(perc)
+        
+rem = 1-sum(percogs_filtered)
+xgs_filtered.append("rest")
+percogs_filtered.append(rem)
+#print(sum(percogs_filtered))
+
+
+            
+##pie chart:
+piefile = f'{outputfolder}/{inputcog}_patternpie.pdf'
+
+#define Seaborn color palette to use
+colors = sns.color_palette('pastel')#[0:5]
+
+#create pie chart
+plt.figure(figsize=(10, 10))
+plt.pie(percogs_filtered, 
+        labels = xgs_filtered, 
+        colors = colors, 
+        #colors=sns.color_palette('Set2'),
+        autopct='%.0f%%', 
+        textprops={'fontsize':8})
+
+plt.title(
+    label=f'{inputcog} ECOG X groups', 
+    fontdict={"fontsize":10},
+    pad=20
+)
+
+plt.savefig(piefile)
+
+
